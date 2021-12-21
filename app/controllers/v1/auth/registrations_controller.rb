42e35e44ab677a404b7f05363cbacc57d1387035
akira-yp <yog.puripuri@gmail.com>
2021-12-21 23:55:51 +0900

    アカウント作成のストロングパラメータを設定

--- app/controllers/v1/auth/registrations_controller.rb
+++ app/controllers/v1/auth/registrations_controller.rb
@@ -1,2 +1,10 @@
 class V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name designer email password))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name))
+  end
 end
