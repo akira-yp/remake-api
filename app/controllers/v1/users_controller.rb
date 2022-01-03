cd8c7466020415be9dc42b2798d5bfd2def5b8ae
akira-yp <yog.puripuri@gmail.com>
2022-01-04 00:05:29 +0900

    デザイナー一覧機能を追加

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,6 +3,9 @@ class V1::UsersController < ApplicationController
 
   def index
     designers = User.where(designer: true).select('id', 'name')
+    designers = designers.map do |designer|
+      { id: designer.id, name: designer.name, avatar: designer.profile.avatar_url}
+    end
     render json: designers
   end
 
